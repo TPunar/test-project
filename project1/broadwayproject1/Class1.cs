9156bfad3b677e540894c96a9a415526e7a4e28d
Punar Tamrakar <tamrakarpunar@gmail.com>
2021-02-12 14:54:33 +0545

    Initial commit

---
+++ project1/broadwayproject1/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using broadwayproject1;
+
+namespace broadwayproject1
+{
+    public class math
+    {
+        public decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+        public decimal Subtract (decimal a, decimal b)
+        {
+            return a - b;
+
+        }
+        public decimal multiply (decimal a , decimal b)
+        {
+            return a * b;
+
+        }
+        public decimal divide ( decimal a, decimal b)
+        {
+            return a / b;
+
+        }
+
+
+    }
+}
---
+++ project1/broadwayproject1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace broadwayproject1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            mathops();
+            Console.ReadLine();
+
+
+        }
+        static void mathops()
+        {
+            Console.Write("Enter the First Number:");
+            var a = Console.ReadLine();
+            Console.Write("Enter the Second Number:");
+            var b = Console.ReadLine();
+
+            var num_a = Convert.ToDecimal(a);
+            var num_b = Convert.ToDecimal(b);
+
+            math Math = new math();
+
+            Console.WriteLine("Addation " + Math.Add(num_a, num_b));
+            Console.WriteLine("Subtraction {0}" , Math.Subtract(num_a, num_b));
+            Console.WriteLine("Multiplication " + Math.multiply(num_a, num_b));
+            Console.WriteLine("Division " + Math.divide(num_a, num_b));
+
+
+        }
+    }
+}
